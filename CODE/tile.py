c0acdc7fea4d04b4c03f1088a40557e2d6d980be
Creepermin-20 <64103311+Creepermin-20@users.noreply.github.com>
2023-06-28 16:09:31 -0500

    Update tile.py

--- CODE/tile.py
+++ CODE/tile.py
@@ -4,6 +4,6 @@ from settings import *
 class Tile(pygame.sprite.Sprite):
     def __init__(self,pos,groups):
         super().__init__(groups)
-        self.image = pygame.image.load("/home/aydan/Documents/GitHub/PFMCM/MEDIA/tiles/wall1.png").convert_alpha()
+        self.image = pygame.image.load("../MEDIA/tiles/wall1.png").convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
         self.hitbox = self.rect.inflate(0,-25)
